269aee33a395058538695f84a2101e171b3609db
oalme004@fiu.edu <oalme004@fiu.edu>
2018-05-17 14:58:27 -0400

    web socket chat module done

---
+++ chat/src/main/java/com/alttube/chat/Model/ChatModel.java
@@ -0,0 +1,14 @@
+package com.alttube.chat.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ChatModel {
+
+    private String sender;
+    private String text;
+}
